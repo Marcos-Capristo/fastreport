ab1561500d48dc793f4f47858092ebf00e32472a
Marcos-Capristo <marcos.capristo@gmail.com>
2022-11-03 00:08:44 -0300

    Write Number-Report

--- js/js.js
+++ js/js.js
@@ -1,14 +1,44 @@
 document.querySelector('#btn-teste').addEventListener('click', function(){
     showModal('#form-model')
 })
+document.querySelectorAll('.btn-close').forEach(element=>{
+    element.addEventListener('click', function(){
+        hideModal()
+    })
+})
 document.querySelector('#btn-model').addEventListener('click', function(){
     hideModal()
     showModal('#form-number-report')
 })
 document.querySelector('#btn-form-number-report').addEventListener('click', function(){
+    writeNumberReport()
     hideModal()
 })
-
+function inverter(num){
+    num1 = num
+    resultado = ''
+    for (i=num1.length-1;i>=0;i--){
+        resultado+=num1[i]
+    }
+    return resultado
+}
+function formatMilhar(num){
+let num1 = inverter(num)
+resultado = ''
+    for(i=0;i<num1.length;i++){
+        resultado += num1[i]
+        if (i%3==2 && i!=num1.length-1){
+            resultado += `.` 
+        }               
+    }
+    return inverter(resultado)
+}
+function writeNumberReport(){
+    const numberReport = formatMilhar(document.querySelector('#input-number-report').value.trim())
+    const designatedDate = new Date(document.querySelector('#input-designated-date').value).getFullYear()
+    texto = `${numberReport}/${designatedDate}`
+    document.querySelector('#article-number-report').innerHTML = `<h1>Laudo ${texto}</h1>`
+}
 function showModal(element_){
     const fade = document.querySelector('#fade').style 
     const modal = document.querySelector(element_).style
@@ -21,7 +51,6 @@ function showModal(element_){
     modal.zIndex = '100'
     modal.transition = '0.5s'
 }
-
 function hideModal(){
     const fade = document.querySelector('#fade').style 
     const modal = document.querySelectorAll('.modal')
